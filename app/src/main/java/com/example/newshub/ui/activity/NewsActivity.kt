f7b468b001aa6eac3cc0ad953eaffa6a88a1d8f4
Paulus-Lawrenzo <paulus.martuatambunan@gmail.com>
2024-02-01 06:15:12 +0700

    Setup Packages, Resources & Permissions

--- app/src/main/java/com/example/newshub/ui/activity/NewsActivity.kt
+++ app/src/main/java/com/example/newshub/ui/activity/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.newshub
+package com.example.newshub.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newshub.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
